65ac55439d8da0efc6578ebd6fbbe29ba3280b4b
Aitor Chicharro <toyouthat@gmail.com>
2022-11-03 12:17:42 +0000

    feat(tsup-config): allow to use postcss modules

--- packages/tsup-config/src/react.ts
+++ packages/tsup-config/src/react.ts
@@ -1,5 +1,7 @@
 import path from "path";
-import fs from "fs";
+import fs, { promises as fsPromises } from "fs";
+import postcss from "postcss";
+import postcssModules from "postcss-modules";
 import type { Options } from "tsup";
 
 // Import package.json from the app directory
@@ -32,5 +34,67 @@ export function defaultOptions(options: Options): Options {
     minify: !options.watch,
     // Inject React imports into the bundle to avoid importing it in each file
     inject: [path.resolve(__dirname, "react-import.js")],
+    // Allow PostCSS modules
+    esbuildPlugins: [
+      {
+        name: "css-module",
+        setup(build): void {
+          build.onResolve(
+            { filter: /\.module\.css$/, namespace: "file" },
+            (args) => ({
+              path: `${args.path}#css-module`,
+              namespace: "css-module",
+              pluginData: {
+                pathDir: path.join(args.resolveDir, args.path),
+              },
+            })
+          );
+          build.onLoad(
+            { filter: /#css-module$/, namespace: "css-module" },
+            async (args) => {
+              const { pluginData } = args as {
+                pluginData: { pathDir: string };
+              };
+
+              const source = await fsPromises.readFile(
+                pluginData.pathDir,
+                "utf8"
+              );
+
+              let cssModule = {};
+              const result = await postcss([
+                postcssModules({
+                  getJSON(_, json) {
+                    cssModule = json;
+                  },
+                }),
+              ]).process(source, { from: pluginData.pathDir });
+
+              return {
+                pluginData: { css: result.css },
+                contents: `import "${
+                  pluginData.pathDir
+                }"; export default ${JSON.stringify(cssModule)}`,
+              };
+            }
+          );
+          build.onResolve(
+            { filter: /\.module\.css$/, namespace: "css-module" },
+            (args) => ({
+              path: path.join(args.resolveDir, args.path, "#css-module-data"),
+              namespace: "css-module",
+              pluginData: args.pluginData as { css: string },
+            })
+          );
+          build.onLoad(
+            { filter: /#css-module-data$/, namespace: "css-module" },
+            (args) => ({
+              contents: (args.pluginData as { css: string }).css,
+              loader: "css",
+            })
+          );
+        },
+      },
+    ],
   };
 }
